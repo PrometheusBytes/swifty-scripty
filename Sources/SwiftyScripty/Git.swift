07fd037b6d15d8cc000d3f0dc8ffe341bc4adea0
Daniele Leto <danieleleto94@gmail.com>
2024-06-06 16:25:51 +0200

    add git feature

--- Sources/SwiftyScripty/Git.swift
+++ Sources/SwiftyScripty/Git.swift
@@ -7,10 +7,9 @@ import Foundation
 /// This protocol specifies methods for checking repository changes, discarding changes, and accessing the root directory of the repository.
 //sourcery: AutoMockable
 public protocol Git {
-    
     /// The root directory of the Git repository.
     var root: String? { get }
-
+    
     /// Checks if there are any changes in the Git repository.
     ///
     /// - Returns: `true` if there are changes in the repository, `false` otherwise.
@@ -18,6 +17,43 @@ public protocol Git {
     
     /// Discards all changes in the Git repository.
     func discardChanges()
+    
+    /// Stages all changes in the Git repository.
+    func stageChanges()
+    
+    /// Commits changes in the Git repository with the specified message.
+    ///
+    /// - Parameter message: The commit message.
+    func commitChanges(message: String)
+    
+    /// Pulls changes from the remote repository.
+    func pull()
+    
+    /// Pushes changes to the remote repository.
+    func push()
+    
+    /// Creates a new branch with the specified name.
+    ///
+    /// - Parameter branchName: The name of the new branch.
+    func createBranch(branchName: String)
+    
+    /// Switches to the specified branch.
+    ///
+    /// - Parameter branchName: The name of the branch to switch to.
+    func switchToBranch(branchName: String)
+    
+    /// Merges the specified branch into the current branch.
+    ///
+    /// - Parameter branchName: The name of the branch to merge.
+    func mergeBranch(branchName: String)
+    
+    /// Deletes the specified branch.
+    ///
+    /// - Parameter branchName: The name of the branch to delete.
+    func deleteBranch(branchName: String)
+    
+    /// Shows the Git log.
+    func showLog()
 }
 
 // MARK: - Git Implementation
@@ -43,5 +79,41 @@ struct GitImpl: Git {
 
     func discardChanges() {
         shell.runZsh(command: "git checkout .")
+    }
+    
+    func stageChanges() {
+        shell.runZsh(command: "git add .")
+    }
+    
+    func commitChanges(message: String) {
+        shell.runZsh(command: "git commit -m \"\(message)\"")
+    }
+    
+    func pull() {
+        shell.runZsh(command: "git pull")
+    }
+    
+    func push() {
+        shell.runZsh(command: "git push")
+    }
+    
+    func createBranch(branchName: String) {
+        shell.runZsh(command: "git checkout -b \(branchName)")
+    }
+    
+    func switchToBranch(branchName: String) {
+        shell.runZsh(command: "git checkout \(branchName)")
+    }
+    
+    func mergeBranch(branchName: String) {
+        shell.runZsh(command: "git merge \(branchName)")
+    }
+    
+    func deleteBranch(branchName: String) {
+        shell.runZsh(command: "git branch -d \(branchName)")
+    }
+    
+    func showLog() {
+        shell.runZsh(command: "git log")
     }
 }
