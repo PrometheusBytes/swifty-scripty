f304c4a0ae1b80ada446c0c0f79b13a278161344
Daniele Leto <danieleleto94@gmail.com>
2024-06-06 16:55:16 +0200

    add bundler doc

--- Sources/SwiftyScripty/Bundler.swift
+++ Sources/SwiftyScripty/Bundler.swift
@@ -1,28 +1,42 @@
 import Foundation
 
-//sourcery: AutoMockable
+/// Protocol defining operations related to Bundler.
+// sourcery: AutoMockable
 public protocol Bundler {
-  func isInstalled(minimumVersion: Version) -> Bool
-  func isInstalled() -> Bool
+    /// Checks if Bundler is installed and if its version meets the specified minimum version requirement.
+    ///
+    /// - Parameter minimumVersion: The minimum version required.
+    /// - Returns: `true` if Bundler is installed and its version meets the minimum requirement, otherwise `false`.
+    func isInstalled(minimumVersion: Version) -> Bool
+    
+    /// Checks if Bundler is installed without considering the version.
+    ///
+    /// - Returns: `true` if Bundler is installed, otherwise `false`.
+    func isInstalled() -> Bool
 }
 
+/// Concrete implementation of the Bundler protocol.
 struct BundlerImpl: Bundler {
-  @Injected(\.shell) var shell
+    /// The shell utility used to execute commands.
+    @Injected(\.shell) var shell
 
-  func isInstalled(minimumVersion: Version) -> Bool {
-    guard let currentVersion = shell.zsh(command: "bundle -v") else {
-      return false
+    func isInstalled(minimumVersion: Version) -> Bool {
+        guard let currentVersion = shell.zsh(command: "bundle -v") else {
+            return false
+        }
+
+        guard let bundlerVersion = Version(text: currentVersion, format: "Bundler version <version>") else {
+            return false
+        }
+
+        shell.print(color: .magenta, text: "Bundler version installed is \(bundlerVersion)")
+        return bundlerVersion >= minimumVersion
     }
 
-    guard let bundlerVersion = Version(text: currentVersion, format: "Bundler version <version>") else {
-      return false
+    /// Checks if Bundler is installed without considering the version.
+    ///
+    /// - Returns: `true` if Bundler is installed, otherwise `false`.
+    func isInstalled() -> Bool {
+        return isInstalled(minimumVersion: .zero)
     }
-
-    shell.print(color: .magenta, text: "Bundler version installed is \(bundlerVersion)")
-    return bundlerVersion >= minimumVersion
-  }
-
-  func isInstalled() -> Bool {
-    isInstalled(minimumVersion: .zero)
-  }
 }
