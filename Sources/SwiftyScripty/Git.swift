53e3f21a5910584ffdd4758a7a3cad6eb9f872e7
Daniele Leto <danieleleto94@gmail.com>
2024-06-06 15:13:01 +0200

    add git implementation

--- Sources/SwiftyScripty/Git.swift
+++ Sources/SwiftyScripty/Git.swift
@@ -1,16 +1,28 @@
 import Foundation
 
+/// Git: A protocol defining common operations for interacting with a Git repository.
+///
+/// This protocol specifies methods for checking repository changes, discarding changes, and accessing the root directory of the repository.
 //sourcery: AutoMockable
 public protocol Git {
+    
+    /// The root directory of the Git repository.
     var srcRoot: String? { get }
 
+    /// Checks if there are any changes in the Git repository.
+    ///
+    /// - Returns: `true` if there are changes in the repository, `false` otherwise.
     func hasRepoChanges() -> Bool
+    
+    /// Discards all changes in the Git repository.
     func discardChanges()
 }
 
+/// GitImpl: A concrete implementation of the Git protocol.
+///
+/// This struct provides implementations for common Git operations using a shell command interface.
 struct GitImpl: Git {
-    @Injected(\.shell)
-    var shell: Shell
+    @Injected(\.shell) var shell: Shell
 
     var srcRoot: String? {
         shell.zsh(command: "git rev-parse --show-toplevel")
