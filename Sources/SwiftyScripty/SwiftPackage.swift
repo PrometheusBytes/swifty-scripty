c68f1624ec22699be75a59fd1c86780ee190d0be
Matteo Cultrera <mcultrera94@icloud.com>
2024-06-10 08:28:53 +0200

    Added Methods for CLI

--- Sources/SwiftyScripty/SwiftPackage.swift
+++ Sources/SwiftyScripty/SwiftPackage.swift
@@ -11,6 +11,12 @@ public protocol SwiftPackage {
     ///
     /// - Parameter path: The URL of the directory where the Swift package should be initialized.
     func initialize(at path: URL)
+
+    /// Initializes a Swift package at the specified path.
+    ///
+    /// - Parameter path: The URL of the directory where the Swift package should be initialized.
+    /// - Returns: The output of the command
+    func runInitialize(at path: URL) -> Command?
 }
 
 // MARK: - SwiftPackage Implementation
@@ -27,5 +33,15 @@ struct SwiftPackageImpl: SwiftPackage {
         let reachFolderCommand = "cd \(path.getFullPath())"
         let initializePackageCommand = "swift package init --type executable"
         shell.runZsh(command: "\(reachFolderCommand);\(initializePackageCommand)")
+    }
+
+    /// Initializes a Swift package at the specified path.
+    ///
+    /// - Parameter path: The URL of the directory where the Swift package should be initialized.
+    /// - Returns: The output of the command
+    func runInitialize(at path: URL) -> Command? {
+        let reachFolderCommand = "cd \(path.getFullPath())"
+        let initializePackageCommand = "swift package init --type executable"
+        return shell.zshWithExitCode(command:"\(reachFolderCommand);\(initializePackageCommand)" )
     }
 }
