db2459782d10eaf7eb72bbc5b6be227fe979690d
Daniele Leto <daniele.leto@telepass.com>
2024-07-13 15:27:35 +0200

    add swiftyscriptymacros target

--- Package.swift
+++ Package.swift
@@ -1,6 +1,7 @@
-// swift-tools-version: 5.7
+// swift-tools-version: 5.9
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
+import CompilerPluginSupport
 import PackageDescription
 
 let package = Package(
@@ -16,15 +17,22 @@ let package = Package(
         .library(
             name: "SwiftyScriptyMocks",
             targets: ["SwiftyScriptyMocks"]
+        ),
+        .library(
+          name: "SwiftyScriptyMacros",
+          targets: ["SwiftyScriptyMacros"]
         )
     ],
-    dependencies: [],
+    dependencies: [
+      .package(url: "https://github.com/apple/swift-syntax.git", from: "510.0.2")
+    ],
     targets: [
         
         // MARK: - Swifty Scripty
         
         .target(
             name: "SwiftyScripty",
+            dependencies: ["SwiftyScriptyMacros"],
             path: "Sources/SwiftyScripty",
             resources: [.copy("Resources")],
             swiftSettings: [.define("DEBUG", .when(configuration: .debug))]
@@ -36,6 +44,17 @@ let package = Package(
             name: "SwiftyScriptyMocks",
             dependencies: ["SwiftyScripty"],
             path: "Mocks"
+        ),
+
+        // MARK:  Swifty Scripty Macros
+
+        .macro(
+            name: "SwiftyScriptyMacros",
+            dependencies: [
+              .product(name: "SwiftSyntaxMacros", package: "swift-syntax"),
+              .product(name: "SwiftCompilerPlugin", package: "swift-syntax")
+            ],
+            path: "Macros"
         ),
 
         // MARK:  Swifty Scripty Test Target
