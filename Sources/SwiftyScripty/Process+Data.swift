f39e32d23eb388812a10a60956042db2193d6f3f
Matteo Cultrera <mcultrera94@icloud.com>
2024-09-27 12:13:50 +0200

    Added New Process

---
+++ Sources/SwiftyScripty/Process+Data.swift
@@ -0,0 +1,86 @@
+import Foundation
+
+/// A `struct` representing a system process
+public struct SwiftyProcess {
+
+    // MARK: - Private Properties
+
+    private let process: Process
+    private let inputPipe: Pipe
+
+    // MARK: - Public Properties
+
+    /// The `AsyncStream` that posts the output of the command while it's executing.
+    public let stream: AsyncStream<Output>
+
+    // MARK: - Init
+
+    init(stream: AsyncStream<Output>, process: Process, inputPipe: Pipe) {
+        self.stream = stream
+        self.process = process
+        self.inputPipe = inputPipe
+    }
+
+    // MARK: - Public Functions
+
+    /// Writes new data to the standard input pipe, allowing the process to read it.
+    ///
+    /// - Parameters:
+    ///   - data: The `Data` to be written in the pipe.
+    ///   - terminateOnFailure: Optional, if `true`, terminates the process if the write fails for any reason.
+    public func write(
+        data: Data,
+        terminateOnFailure: Bool = false
+    ) {
+        do {
+            try writeData(data, in: inputPipe)
+        } catch {
+            if terminateOnFailure {
+                process.terminate()
+            }
+        }
+    }
+
+    /// Writes new data to the standard input pipe, allowing the process to read it.
+    ///
+    /// - Parameters:
+    ///   - string: The `String` to be written in the pipe, using the `utf8` encoding.
+    ///   - terminateOnFailure: Optional, if `true`, terminates the process if the write fails for any reason.
+    public func write(
+        string: String,
+        terminateOnFailure: Bool = false
+    ) {
+        do {
+            guard let data = string.data(using: .utf8) else { throw NSError() }
+
+            try writeData(data, in: inputPipe)
+        } catch {
+            if terminateOnFailure {
+                process.terminate()
+            }
+        }
+    }
+
+    // MARK: - Private Functions
+
+    private func writeData(_ data: Data, in pipe: Pipe) throws {
+        try pipe.fileHandleForWriting.write(contentsOf: data)
+        try pipe.fileHandleForWriting.close()
+    }
+}
+
+// MARK: - Output
+
+public extension SwiftyProcess {
+    /// The enum representing the output of the process.
+    enum Output {
+        /// Regular output on the `standardOutput` pipe.
+        case output(String)
+        /// Error output on the `standardError` pipe.
+        case error(String)
+        /// Error thrown on creating the process.
+        case failureRunningProcess(Error)
+        /// Termination code of the process.
+        case exitCode(Int32)
+    }
+}
---
+++ Sources/SwiftyScripty/Process.swift
@@ -0,0 +1,128 @@
+import Foundation
+
+// MARK: - ProcessRunner Protocol
+
+/// A protocol modelling a runnable process of the system.
+//sourcery: AutoMockable
+public protocol ProcessRunner {
+    /// Runs the passed command in a new process.
+    ///
+    /// - Parameters:
+    ///   - command: The command to be executed in the process.
+    ///   - shellType: The shell to be used to run the command.
+    func run(
+        command: String,
+        shellType: ShellType
+    ) -> SwiftyProcess
+}
+
+public extension ProcessRunner {
+    /// Runs the passed command in a new process.
+    ///
+    /// - Parameters:
+    ///   - command: The command to be executed in the process.
+    ///   - shellType: The shell to be used to run the command. Defaults to `zsh`
+    func run(
+        command: String,
+        shellType: ShellType = .zsh
+    ) -> SwiftyProcess {
+        run(command: command, shellType: shellType)
+    }
+}
+
+// MARK: - ProcessRunner Implementation
+
+struct ProcessRunnerImpl: ProcessRunner {
+    // MARK: - Properties
+
+    /// A token used to identify the start of an interactive shell script output.
+    let scriptStartToken = "<--Swift Script Interactive Shell Start-->"
+}
+
+// MARK: - ProcessRunner Protocol Conformance
+
+extension ProcessRunnerImpl {
+    func run(command: String, shellType: ShellType) -> SwiftyProcess {
+        let newCommand = "echo \"\(scriptStartToken)\"; " + command
+        let (process, pipe, errorPipe, inputPipe) = configureStandardProcessAndPipe(
+            command: newCommand,
+            shellType: shellType
+        )
+
+        let stream = AsyncStream<SwiftyProcess.Output> { continuation in
+            defer { continuation.finish() }
+            var shouldStartOutput = false
+
+            pipe.fileHandleForReading.readabilityHandler = { pipe in
+                guard let data = String(data: pipe.availableData, encoding: .utf8), !data.isEmpty else { return }
+
+                guard shouldStartOutput else {
+                    if data == scriptStartToken {
+                        shouldStartOutput = true
+                    }
+
+                    return
+                }
+
+                continuation.yield(.output(data))
+            }
+
+            errorPipe.fileHandleForReading.readabilityHandler = { pipe in
+                guard let data = String(data: pipe.availableData, encoding: .utf8), !data.isEmpty else { return }
+
+                continuation.yield(.error(data))
+            }
+
+            do {
+                try process.run()
+                moveToForeground(process)
+                process.waitUntilExit()
+                restoreDefaultForegroundProcess()
+                continuation.yield(.exitCode(process.terminationStatus))
+                return
+            } catch {
+                continuation.yield(.failureRunningProcess(error))
+                return
+            }
+        }
+
+        return SwiftyProcess(
+            stream: stream,
+            process: process,
+            inputPipe: inputPipe
+        )
+    }
+}
+
+// MARK: - Private Helpers
+
+private extension ProcessRunnerImpl {
+    private func configureStandardProcessAndPipe(
+        command: String,
+        shellType: ShellType
+    ) -> (process: Process, pipe: Pipe, errorPipe: Pipe, outputPipe: Pipe) {
+        let process = Process()
+        let pipe = Pipe()
+        let errorPipe = Pipe()
+        let outputPipe = Pipe()
+
+        process.standardOutput = pipe
+        process.standardError = errorPipe
+        process.launchPath = shellType.rawValue
+        #if DEBUG
+        process.arguments = ["-i", "-l", "-c", command]
+        #else
+        process.arguments = ["-c", command]
+        #endif
+        return (process, pipe, errorPipe, outputPipe)
+    }
+
+    private func moveToForeground(_ process: Process) {
+        tcsetpgrp(STDIN_FILENO, process.processIdentifier)
+        signal(SIGTTOU, SIG_IGN)
+    }
+
+    private func restoreDefaultForegroundProcess() {
+        tcsetpgrp(STDIN_FILENO, getpid())
+    }
+}
